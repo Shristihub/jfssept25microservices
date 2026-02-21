dab9e4527c2bdff70047868fa6705adede5e5d81
shristitechacademy <sripriyamathan@gmail.com>
2026-02-21 12:59:21 +0530

    microservices

---
+++ shipkart-productcatalog-service/src/main/java/com/productcatalog/repository/IProductRepository.java
@@ -0,0 +1,14 @@
+package com.productcatalog.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.productcatalog.model.entities.Product;
+
+public interface IProductRepository extends JpaRepository<Product, Integer>{
+
+	@Query(value = "select p from Product p join p.categories c where c.categoryName=?1")
+	List<Product> findByCategory(String category);
+}
